ad801d33aa4738c09f657bb61fe87327be5fdb1e
vidhu20jan <vidhu20jan@gmail.com>
2024-05-13 11:51:31 +0530

    files updated excec"

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    let userInput = document.querySelector("#userInput");
+     let message = document.querySelector("#message");
+ message.innerHTML="Hello welcome to my wepage " + userInput.value;
+ }
